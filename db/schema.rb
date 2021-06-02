03740dc9ae320615e2b763be537b02260ee36d36
ME <forzamaks@gmail.com>
2021-06-02 18:06:58 +0300

    Add seeds, create test class, create user instance class

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_06_01_165347) do
+ActiveRecord::Schema.define(version: 2021_06_02_142000) do
 
   create_table "answers", force: :cascade do |t|
     t.string "title", null: false
@@ -50,6 +50,7 @@ ActiveRecord::Schema.define(version: 2021_06_01_165347) do
     t.string "password", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "tests"
   end
 
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,39 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+Category.create([
+  {title: 'Frontend'},
+  {title: 'Backend'},
+  {title: 'Mobile Development'},
+  {title: 'Machine learning'}
+])
+
+Test.create([
+  {title: 'HTML', level: 1, category_id: 1},
+  {title: 'Ruby', level: 2, category_id: 2},
+  {title: 'JavaScript', level: 2, category_id: 1},
+  {title: 'GO', level: 3, category_id: 3},
+  {title: 'Node', level: 2, category_id:  2}
+])
+
+Question.create([
+  {body: 'Что такое HTML?', test_id: 1},
+  {body: 'Какие основные виды списков применяют при создании веб-страницы?', test_id: 1},
+  {body: 'Что такое прототип объекта?', test_id: 3},
+  {body: 'Перечислите некоторые особенности Ruby?', test_id: 2},
+  {body: 'Как создать объект Ruby?', test_id: 2}
+])
+
+Answer.create([
+  {title: 'Вариант 1', question_id: 1},
+  {title: 'Вариант 2', question_id: 1},
+  {title: 'Вариант 3', correct: true, question_id: 1},
+  {title: 'Вариант 4', question_id: 1}
+])
+
+User.create([
+  {name: 'test', email: 'test@test.test', password: 'password', tests: [1, 2, 3]},
+  {name: 'admin', email: 'admin@test.test', password: 'admin_pass'},
+])
