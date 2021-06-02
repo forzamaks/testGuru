f2fa5f04564f20fd15787735b49827f79f3e1579
ME <forzamaks@gmail.com>
2021-06-02 21:56:08 +0300

    Add order from request

--- app/models/test.rb
+++ app/models/test.rb
@@ -1,7 +1,7 @@
 class Test < ApplicationRecord
   
   def self.all_test_of_category(category)
-    Test.joins('JOIN categories ON test.category_id = categories.id').where('categories.title = :title', title: category)
+    Test.joins('JOIN categories ON test.category_id = categories.id').where('categories.title = :title', title: category).order(id: desc)
   end
 
 end
