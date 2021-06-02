357dfa088d9cebbc3e9b040dd06010409bd82d2a
ME <forzamaks@gmail.com>
2021-06-02 21:48:57 +0300

    Add test_users table, fix requests in models, fix seeds file

--- db/migrate/20210602142000_add_user_test_column.rb
+++
@@ -1,5 +0,0 @@
-class AddUserTestColumn < ActiveRecord::Migration[5.2]
-  def change
-    add_column :users, :tests, :string
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_06_02_142000) do
+ActiveRecord::Schema.define(version: 2021_06_02_182530) do
 
   create_table "answers", force: :cascade do |t|
     t.string "title", null: false
@@ -44,13 +44,21 @@ ActiveRecord::Schema.define(version: 2021_06_02_142000) do
     t.index ["category_id"], name: "index_tests_on_category_id"
   end
 
+  create_table "tests_users", id: false, force: :cascade do |t|
+    t.integer "user_id", null: false
+    t.integer "test_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["test_id"], name: "index_tests_users_on_test_id"
+    t.index ["user_id"], name: "index_tests_users_on_user_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "name", null: false
     t.string "email", null: false
     t.string "password", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "tests"
   end
 
 end
