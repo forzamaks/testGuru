a2e26e942d7295d570d76f7fd729575c6b4b581b
ME <forzamaks@gmail.com>
2021-06-03 17:15:00 +0300

    Set associations in models

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,8 @@
 class User < ApplicationRecord
 
+  has_many :tests
+  has_and_belongs_to_many :tests
+
   def user_tests_for_level(level)
     Test.where(level: level).joins('JOIN tests_users ON tests_users.test_id = tests.id').where(tests_users: {user_id: id})
   end
--- db/migrate/20210602182530_create_join_table_users_tests.rb
+++ db/migrate/20210602182530_create_join_table_users_tests.rb
@@ -3,8 +3,8 @@ class CreateJoinTableUsersTests < ActiveRecord::Migration[5.2]
     create_join_table :users, :tests do |t|
       t.references :user, index: true, null: false, foreign_key: true
       t.references :test, index: true, null: false, foreign_key: true
-      t.index [:test_id, :user_id]
-      t.index [:user_id, :test_id]
+      t.index [:test_id, :user_id], unique: true
+      # t.index [:user_id, :test_id], unique: true
 
       t.timestamps
     end
