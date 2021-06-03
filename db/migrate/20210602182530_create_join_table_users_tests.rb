8e303dcf547ccb3b987d4dad795e019859eb0545
ME <forzamaks@gmail.com>
2021-06-03 12:10:28 +0300

    Change migration

--- db/migrate/20210602182530_create_join_table_users_tests.rb
+++ db/migrate/20210602182530_create_join_table_users_tests.rb
@@ -1,8 +1,10 @@
 class CreateJoinTableUsersTests < ActiveRecord::Migration[5.2]
   def change
     create_join_table :users, :tests do |t|
-      t.index :user_id
-      t.index :test_id
+      t.references :user, index: true, null: false, foreign_key: true
+      t.references :test, index: true, null: false, foreign_key: true
+      t.index [:test_id, :user_id]
+      t.index [:user_id, :test_id]
 
       t.timestamps
     end
