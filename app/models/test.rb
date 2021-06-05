14bc9e03023e5b111986e15b614a6e9def86b3dc
ME <forzamaks@gmail.com>
2021-06-05 15:45:15 +0300

    Update test.rb

--- app/models/test.rb
+++ app/models/test.rb
@@ -2,11 +2,11 @@ class Test < ApplicationRecord
 
   belongs_to :category
   has_many :questions
-  belongs_to :author, class_name: 'User'
+  belongs_to :author, class_name: 'User', :foreign_key => 'user_id'
   has_and_belongs_to_many :users
 
   def self.all_test_of_category(category)
-    Test.joins(:category).where(title: category).order(id: desc)
+    Category.joins(:tests).where(title: category).order(id: :desc)
   end
 
 end
