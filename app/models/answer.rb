c0ba2f87dba9f0dfcf05e4524acd5a2d8d1af14d
ME <forzamaks@gmail.com>
2021-06-06 09:09:31 +0300

    Fix validations^ add uniq index

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -3,7 +3,20 @@ class Answer < ApplicationRecord
   belongs_to :question
 
   scope :correct, -> { where(correct: true) }
+  scope :join_question, -> { joins(:question) }
 
   validates :title, presence: true
+
+
+  validate :validate_answer_count, on: :create
+
+
+  private
+
+  def validate_answer_count
+
+    answers = Answer.join_question.where(question_id: id)
+    errors.add(:question) if answers.count > 4
+  end
 
 end
--- app/models/category.rb
+++ app/models/category.rb
@@ -2,7 +2,6 @@ class Category < ApplicationRecord
   has_many :tests
 
   default_scope { order(title: :asc) }
-  scope :category_tests, -> (category_name) { joins(:tests).where(title: category_name).order(id: :desc) }
 
   validates :title, presence: true
 
