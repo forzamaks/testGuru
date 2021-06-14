c1a18ee1b328a2b49b6ed1e3f3cf73859b7948db
ME <forzamaks@gmail.com>
2021-06-14 23:45:38 +0300

    add admin panel

--- app/controllers/tests_controller.rb
+++ app/controllers/tests_controller.rb
@@ -1,45 +1,11 @@
 class TestsController < ApplicationController
 
-  before_action :find_test, only: [:destroy, :update, :edit, :show, :start]
+  before_action :authenticate_user!
+  before_action :find_test, only: [:start]
   before_action :set_user, only: :start
 
   def index
     @tests = Test.all
-  end
-
-  def show
-
-  end
-
-  def new
-    @test = Test.new
-  end
-
-  def create
-    @test = Test.new(test_params)
-
-    if @test.save
-      redirect_to @test
-    else
-      render :new
-    end
-  end
-
-  def edit
-    
-  end
-
-  def update
-    if @test.update(test_params)
-      redirect_to @test
-    else
-      render :edit
-    end
-  end
-
-  def destroy
-    @test.destroy
-    redirect_to tests_path
   end
 
   def start
@@ -51,10 +17,6 @@ class TestsController < ApplicationController
 
   def find_test
     @test = Test.find(params[:id])
-  end
-
-  def test_params
-    params.require(:test).permit(:title, :level, :category_id, :user_id)
   end
 
   def set_user
