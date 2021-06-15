473fd7fe7bf43e4d44fe812e0af5d52a0ff1e065
ME <forzamaks@gmail.com>
2021-06-15 11:43:57 +0300

    Fix

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,7 +7,7 @@ class ApplicationController < ActionController::Base
 
   def after_sign_in_path_for(resource)
     flash[:notice] = "Welcome! #{current_user.name} #{current_user.last_name}"
-    current_user.is_a?(Admin) ? admin_tests_path : root_path
+    current_user.admin? ? admin_tests_path : root_path
   end
 
   def configure_permitted_parameters
--- app/models/admin.rb
+++ app/models/admin.rb
@@ -1,5 +1,3 @@
 class Admin < User
 
-  
-
 end
