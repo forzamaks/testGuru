08759a9ee6842a5c671d63d666c6b506c7edde41
ME <forzamaks@gmail.com>
2021-06-18 23:21:40 +0300

    Fix

--- db/migrate/20210618063135_add_index_user_question_to_gists.rb
+++
@@ -1,8 +0,0 @@
-class AddIndexUserQuestionToGists < ActiveRecord::Migration[5.2]
-  def change
-    change_table :gists do |t|
-      t.references :user, foreign_key: true
-      t.references :question, foreign_key: true
-    end
-  end
-end
